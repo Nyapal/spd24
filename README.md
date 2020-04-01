10bcf337ccd46f1606bc4807fd398f5003828b2e
Nyapal <nyapal14@gmail.com>
2020-03-31 18:42:18 -0700

    this solution isnt all the way there but im tired

---
+++ README.md
@@ -0,0 +1,2 @@
+# SPD 2.4 Technical Interviewing
+Nyapal's completed technical interview questions from SPD 2.4
---
+++ problemOne/solution.py
@@ -0,0 +1,18 @@
+# Problem 1
+# Given an array a of n numbers and a target value t, find two numbers whose sum is t.
+# Example: a=[5, 3, 6, 8, 2, 4, 7], t=10  [3, 7] or [6, 4] or [8, 2]
+
+def problemOne (n, t):
+    output = []
+    for num in n:
+        possible_sum = t - num 
+
+        if possible_sum in n:
+            output.append(num)
+            output.append(possible_sum)
+        # print(num)
+    return output
+
+if __name__ == "__main__":
+    solution = problemOne([5, 4, 2, 7], 6)
+    print(solution)
